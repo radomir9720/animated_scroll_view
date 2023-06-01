0383d95bba93c1f8603a9f2853d17030f964473c
Radomir Epur <radomir9720@gmail.com>
2023-06-01 16:30:03 +0300

    style(widgetbook_app): removed redundant commented lines

--- widgetbook_app/lib/widgets/scrollables/models/my_model.dart
+++ widgetbook_app/lib/widgets/scrollables/models/my_model.dart
@@ -5,12 +5,10 @@ class MyModel {
   const MyModel({
     required this.id,
     required this.color,
-    // required this.index,
   });
 
   final Color color;
   final int id;
-  // final int index;
 
   @override
   String toString() => 'MyModel(color: $color, id: $id)';
